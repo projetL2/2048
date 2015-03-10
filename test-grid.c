a157f7ab25006e709c7674568081ccd035adb353
Chrystelle Petureau <petur001@emi.u-bordeaux1.fr>
2015-03-10 12:02:34 +0100

    mise à jour du fichier test et suppression de nouvelle branche

--- test-grid.c
+++ test-grid.c
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+//ce fichier test est creer dans le but de tester la création, copie, supression et biensur l'affichage de la grille.
+//ce programme à été lancer avec valgrind pour verifier les fuites de memoires.
 
-//fonction pour tester notre grille et son affichage
-
+//fonction obligatoire pour tester notre grille et son affichage
 void afficher (grid g) {
 
     printf (" +~~~~+~~~~+~~~~+~~~~+ \n ");
@@ -29,32 +30,49 @@ void afficher (grid g) {
 int main (void) {
 
     grid g = new_grid (); //creation d'une nouvelle grille
-
-
     srand(time (NULL)); // srand est une fonction de la bibliothéque stdlib.h.
 			// cette ligne permet d'initiliser la fonction time (de la bibliotheque time.h) 
     
     afficher(g);
     add_tile(g);
-	afficher(g);
+    afficher(g);
+    //test des mouvement de façon aléatoire (le hasard choisi toujours mieux)
+    /*printf("on rentre dans la boucle de test des mouvements\n")	
     for(int i = 0; i<10; i++) { // creation d'une boucle pour tester add_tile 
 		 		
 		int dirRand= rand()%3; // valariable pour choisir la direction de facon aléatoire
+		printf("le mouvement est %d (0->droite, 1->bas,2->haut,3->gauche) \n",dirRand);
 		switch (dirRand){
-			case 0:
-				do_move(g,LEFT);
-			break;
-			case 1:
+			case 0:	
 				do_move (g,RIGHT);
 			break;
-			case 2:
+			case 1:
 				do_move (g,DOWN);
+			break;
+			case 2:
+				do_move(g,UP);
+			break;
+			case 3:
+				do_move(g,LEFT);
 			break;
 		}
 		add_tile(g);
 		afficher(g);
+		printf("on est au %d tours\n",i);
 	}
+	printf("on est sorti de la boucle);*/
+	//test de copy_grid
+	printf("on essaye de copier la grille \n");
+	afficher (g);
+	grid gBis= new_grid();
+	copy_grid (g,gBis);
+	afficher (gBis);
 
-
-    return EXIT_SUCCESS;
+	//test de delete (avec valgrind au lancement)
+	printf("on essaye de supprimer les 2 grilles, d'abords g \n");
+	delete_grid(g);
+	printf("on a supprimé g, on supprime gBis\n");
+	delete_grid(gBis);
+	printf("on a fini\n");
+	return EXIT_SUCCESS;
 }
