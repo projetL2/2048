33f5f173d0b99378365c3e53fca9883309bf7fbe
Chrystelle Petureau <petur001@emi.u-bordeaux1.fr>
2015-03-11 10:58:32 +0100

    mise à jour des ficher test

--- Makefile
+++ Makefile
@@ -7,10 +7,10 @@ CC= gcc
 CFLAGS= -std=c99 -Wall -g
 
 #le nom du fichier executable
-OUTFILE= test-grid
+OUTFILE=  test-grid
 
 #les fichiers objets
-OBJS= grid.o test-grid.o
+OBJS= grid.o  test-grid.o 
 
 #les bibliotheques utilisees
 LDLIBS= -lm
--- test-grid.c
+++ test-grid.c
@@ -18,7 +18,7 @@ void afficher (grid g) {
             if (res == 0)
                 printf ("    |"); // on affiche pas 0 pour plus de lisibilité.
             else
-                printf ("%4d|",res); //on reserve la place de 4chiffres quel que soit le nombre réel de 						// la tile. 
+                printf ("%4d|",res); //on reserve la place de 4chiffres quel que soit le nombre réel de la tile. 
         }
         printf ("\n +~~~~+~~~~+~~~~+~~~~+ \n ");
     }
@@ -29,19 +29,34 @@ void afficher (grid g) {
 
 int main (void) {
 
-    grid g = new_grid (); //creation d'une nouvelle grille
-    srand(time (NULL)); // srand est une fonction de la bibliothéque stdlib.h.
-			// cette ligne permet d'initiliser la fonction time (de la bibliotheque time.h) 
-    
-    afficher(g);
-    add_tile(g);
-    afficher(g);
-    //test des mouvement de façon aléatoire (le hasard choisi toujours mieux)
-    /*printf("on rentre dans la boucle de test des mouvements\n")	
-    for(int i = 0; i<10; i++) { // creation d'une boucle pour tester add_tile 
-		 		
+	grid g = new_grid (); //creation d'une nouvelle grille
+	srand(time (NULL)); // srand est une fonction de la bibliothéque stdlib.h. cette ligne permet d'initiliser la fonction time (de la bibliotheque time.h)
+	
+	//test de l'ajout de façon aléatoire (présant pour pour les autres tests)
+	afficher(g);
+	add_tile(g);
+	afficher(g);
+
+	//test de copy_grid
+	/*printf("on essaye de copier la grille \n");
+	afficher (g);
+	grid gBis= new_grid();
+	copy_grid (g,gBis);
+	afficher (gBis);*/
+
+	//test de delete (avec valgrind au lancement)
+	/*printf("on essaye de supprimer les 2 grilles, d'abords g \n");
+	delete_grid(g);
+	printf("on a supprimé g, on supprime gBis\n");
+	delete_grid(gBis);
+	printf("on a fini\n");*/
+
+	//test des mouvement de façon aléatoire (le hasard choisi toujours mieux)
+	/*printf("on rentre dans la boucle de test des mouvements\n");	
+	for(int i = 0; i<10; i++) { // creation d'une boucle pour tester add_tile 
+			
 		int dirRand= rand()%3; // valariable pour choisir la direction de facon aléatoire
-		printf("le mouvement est %d (0->droite, 1->bas,2->haut,3->gauche) \n",dirRand);
+		printf("le mouvement que l'on va faire est %d (0->droite, 1->bas,2->haut,3->gauche) \n",dirRand);
 		switch (dirRand){
 			case 0:	
 				do_move (g,RIGHT);
@@ -60,19 +75,18 @@ int main (void) {
 		afficher(g);
 		printf("on est au %d tours\n",i);
 	}
-	printf("on est sorti de la boucle);*/
-	//test de copy_grid
-	printf("on essaye de copier la grille \n");
-	afficher (g);
-	grid gBis= new_grid();
-	copy_grid (g,gBis);
-	afficher (gBis);
+	printf("on est sorti de la boucle");*/
 
-	//test de delete (avec valgrind au lancement)
-	printf("on essaye de supprimer les 2 grilles, d'abords g \n");
-	delete_grid(g);
-	printf("on a supprimé g, on supprime gBis\n");
-	delete_grid(gBis);
-	printf("on a fini\n");
+	//test sur les acceptations (ou non) de mouvement de can_move et les mouvement réellement effectués
+	//add_tile rempli une case de façon aléatoire, on demande un mouvement en particulier
+	// à nous visuellement de verifié si can_move devait l'accepeter ou non
+	if(can_move(g,RIGHT)){ // tester tour à tour avec RIGHT,DOWN, UP, LEFT
+    		do_move(g,RIGHT);
+   		add_tile(g);
+    		printf("\n le mouvement est valide \n \n");
+   
+  	}
+	
+
 	return EXIT_SUCCESS;
 }
