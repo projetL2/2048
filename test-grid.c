0e16ec8386820a51f6dec52477613fe47a92ce9e
Galmyre <zazarlaliche@gmail.com>
2015-03-12 12:47:24 +0100

    Il y a maintenant un test d'entrée utilisateur, avec des chiffres à rentrer en console, il marche, par contre j'ai trouvé d'autres bugs dans do_move à corriger

--- test-grid.c
+++ test-grid.c
@@ -10,10 +10,10 @@
 void afficher (grid g) {
 
     printf (" +~~~~+~~~~+~~~~+~~~~+ \n ");
-
-    for (int colonne=0; colonne<GRID_SIDE; ++colonne) {
+    
+    for (int ligne=0; ligne<GRID_SIDE; ++ligne) {
         printf("|");
-        for (int ligne=0; ligne<GRID_SIDE; ++ligne) {
+		for (int colonne=0; colonne<GRID_SIDE; ++colonne) {
             tile res = (get_tile (g,colonne,ligne));
             if (res == 0)
                 printf ("    |"); // on affiche pas 0 pour plus de lisibilité.
@@ -32,26 +32,38 @@ int main (void) {
 	grid g = new_grid (); //creation d'une nouvelle grille
 	srand(time (NULL)); // srand est une fonction de la bibliothéque stdlib.h. cette ligne permet d'initiliser la fonction time (de la bibliotheque time.h)
 	
-	//test de l'ajout de façon aléatoire (présant pour pour les autres tests)
-	afficher(g);
-	//add_tile(g);
-	//afficher(g);
+	//***********************************************************************//
+	//test de l'ajout de façon aléatoire (présent pour pour les autres tests)//
+	//***********************************************************************//
+	
+	/*afficher(g);
+	add_tile(g);
+	afficher(g);*/
 
-	//test de copy_grid
+	//******************//
+	//test de copy_grid //
+	//******************//
+	
 	/*printf("on essaye de copier la grille \n");
 	afficher (g);
 	grid gBis= new_grid();
 	copy_grid (g,gBis);
 	afficher (gBis);*/
 
-	//test de delete (avec valgrind au lancement)
+	//*******************************************//
+	//test de delete (avec valgrind au lancement)//
+	//*******************************************//
+	
 	/*printf("on essaye de supprimer les 2 grilles, d'abords g \n");
 	delete_grid(g);
 	printf("on a supprimé g, on supprime gBis\n");
 	delete_grid(gBis);
 	printf("on a fini\n");*/
 
-	//test des mouvement de façon aléatoire (le hasard choisi toujours mieux)
+	//***********************************************************************//
+	//test des mouvement de façon aléatoire (le hasard choisi toujours mieux)//
+	//***********************************************************************//
+	
 	/*printf("on rentre dans la boucle de test des mouvements\n");	
 	for(int i = 0; i<10; i++) { // creation d'une boucle pour tester add_tile 
 			
@@ -77,9 +89,12 @@ int main (void) {
 	}
 	printf("on est sorti de la boucle");*/
 
-	//test sur les acceptations (ou non) de mouvement de can_move et les mouvement réellement effectués
-	//add_tile rempli une case de façon aléatoire, on demande un mouvement en particulier
-	// à nous visuellement de verifié si can_move devait l'accepeter ou non
+	//*************************************************************************************************//
+	//test sur les acceptations (ou non) de mouvement de can_move et les mouvement réellement effectués//
+	//*************************************************************************************************//
+	
+	//add_tile remplit une case de façon aléatoire, on demande un mouvement en particulier
+	// à nous visuellement de verifier si can_move devait l'accepter ou non
 	// tester tour à tour avec RIGHT,DOWN, UP, LEFT
 	/*if(can_move(g,RIGHT)){ 
     		do_move(g,RIGHT);
@@ -88,8 +103,11 @@ int main (void) {
    
   	}*/
 
-	// test de game over
+	//******************//
+	// test de game over//
+	//******************//
 	
+	/*
 	for (int colonne=0; colonne<GRID_SIDE; ++colonne){
 		for (int ligne=0; ligne<GRID_SIDE; ++ligne){
 			set_tile (g,colonne, ligne, colonne+ligne+1);//colone+ligne donnera un nombre différent à chaque fois
@@ -114,7 +132,42 @@ int main (void) {
 		}
 	play(g, dirRand);
 	afficher(g);
+	*/
 	
+	//***************************//
+	//Tests d'entrées utilisateur//
+	//***************************//
+	
+	/*
+	int a; // variable int qui contient le choix utilisateur
+	dir d; // la direction qui va correspondre
+	
+	add_tile(g);
+	afficher(g);
 
+	printf("droite = 0	gauche = 1	haut = 2	bas = 3\n"); // propositions
+	
+	for (int i=0; i<10; i++) { // on va boucler 10 fois pour le test
+		
+		scanf("%d", &a); // récupère un int dans la variable a 		
+		switch (a){  // switch qui fait correspondre l'int au dir
+			case 0:	
+				d=RIGHT;
+			break;
+			case 1:
+				d=LEFT;
+			break;
+			case 2:
+				d=UP;
+			break;
+			case 3:
+				d=DOWN;
+			break;
+		}
+		play(g,d);
+		afficher(g);
+		printf("droite = 0	gauche = 1	haut = 2	bas = 3\n"); // propositions
+	}
+	*/
 	return EXIT_SUCCESS;
 }
